8663b459d8618c3c1df936c7fd2655a21ced469c
Anna Oleks <annaoleks88@gmail.com>
2025-01-15 11:38:25 +0200

    Add project files.

---
+++ Kutse/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kutse.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Ootan sind minu peole! Palun tule!!!";
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 10 ? "Tere hommikust!" : "Tere päevast!";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult Ankeet()
+        {
+            return View();
+        }
+    }
+}
---
+++ Kutse/Models/Guest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Kutse.Models
+{
+    public class Guest
+    {
+        [Required(ErrorMessage = "Sissesta nimi")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Sissesta email")]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "Valesti sissestatud email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Sissesta telefoni number")]
+        [RegularExpression(@"\+372.+", ErrorMessage = "Numbri alguses peab olema +372")]
+        public string Phone {  get; set; }
+        [Required(ErrorMessage = "Sissesta oma valik")]
+
+        public bool? WillAttend { get; set; }
+
+    }
+}
